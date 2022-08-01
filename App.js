1e50f1e5356bd8177a39ab435fff32092453701e
shubhamdhakate <97968232+shubhamdhakate@users.noreply.github.com>
2022-08-01 11:09:00 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-const express = require('express');
-const dada = express();
-dada.get('/api/main', function(req,res) {
-    //res.send("Hi, Express JS");
-    //res.send("<h1>Hi, Express JS HTML Tag</h1>");
-    res.send({
-        name : "shubha..m dhakate",
-        age : 22,
-        job :"software engineer",
-    });
-    
+//Create two middlewares one should be applicable to all the routes present in backend application is containing and other should be applicable to any 2 specific routes 
+
+const express =require("express")
+const app = express()
+
+const middleware1 = (req,res,next) =>{
+    console.log('middleware1')
+    next()
+}
+const middleware2 = (req,res,next) =>{
+    console.log('middleware2')
+    next()
+}
+
+app.use(middleware2)//using middleware2 globally
+app.use(middleware1)
+
+app.get('/page1',middleware1,function (req,res) {
+    console.log('page1 - both middleware 1 and 2 applied')
+})//here we will get both middleware 1 and 2
+
+app.get('/page2',function (req,res) {
+    console.log('page2')
 })
-dada.listen(9090, function() {
-    console.log("Server nning");
-    
+
+app.get('/page3',middleware1,function (req,res) {
+    console.log('page3 - - both middleware 1 and 2 applied')
+})
+
+app.get('/page4',function (req,res) {
+    console.log('page4')
+})
+
+app.get('/page5',function (req,res) {
+    console.log('page5')
+})
+
+app.listen(8080,function () {
+    console.log('App is running')
 })
